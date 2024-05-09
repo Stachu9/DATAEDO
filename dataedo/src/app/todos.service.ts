b319ce903cac3d914ac68105566d9ed0601fc385
stanislaw <starodfirma@gmail.com>
2024-05-09 23:49:39 +0200

    Second element withdrawn

--- dataedo/src/app/todos.service.ts
+++ dataedo/src/app/todos.service.ts
@@ -2,24 +2,39 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 
+interface Todo {
+  id: number,
+  user_id: number,
+  title: string,
+  due_on: string,
+  status: "pending" | "complete"
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
   constructor(private httpClient: HttpClient) {}
 
-  private readonly todoURL = 'https://gorest.co.in/public/v2/todos';
+  private readonly todoURL: string = 'https://gorest.co.in/public/v2/todos';
 
-  getTodos(): Observable<Array<any>> {
-    return this.httpClient.get<Array<Object>>(this.todoURL);
+  getTodos(): Observable<Array<Todo>> {
+    return this.httpClient.get<Array<Todo>>(this.todoURL);
   }
 
-  getSecondPendingTodo() {
+  getPendingTodos(): Observable<Array<Todo>> {
     return this.getTodos().pipe(
-      map(array => {
-        array.filter((object) => {object["status"] === "pending"})
-      })
-    )
+      map(
+        (todos: Array<Todo>) =>
+          todos.filter((todo: Todo): boolean => todo.status === "pending")
+      )
+    );
+  }
+
+  getSecondPendingTodo(): Observable<Todo | null> {
+    return this.getPendingTodos().pipe(
+      map((todos: Array<Todo>) => todos.length >= 2 ? todos[1] : null)
+    );
   }
 
 }
